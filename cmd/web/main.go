a4c092d472ab0628081c5ed4148754dcd767daf1
mr-medrano <rainier.medrano22@gmail.com>
2024-04-17 19:00:18 +0800

    added database connection using pgx

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,24 +1,48 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
 	"net/http"
 	"os"
 	"time"
+
+	"examples.mrmedano.todo/internal/database"
 )
+
+type config struct {
+	addr string
+	dsn  string
+}
 
 type Application struct{}
 
 func main() {
+	var cfg config
+
+	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.dsn, "dsn", "postgres://admin:password@localhost:5432/postgres", "PSQL data source name")
+
+	flag.Parse()
+
 	app := &Application{}
 
+	pgPool, err := database.NewPGXPool(context.Background(), cfg.dsn)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	defer pgPool.Close()
+
 	srv := &http.Server{
-		Addr:         ":9000",
+		Addr:         cfg.addr,
 		Handler:      app.routes(),
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	}
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	fmt.Println(err.Error())
 	os.Exit(1)
 
---
+++ internal/database/dabatase.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func NewPGXPool(ctx context.Context, dsn string) (*pgxpool.Pool, error) {
+	pool, err := pgxpool.New(ctx, dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = pool.Ping(ctx)
+	if err != nil {
+		pool.Close()
+		return nil, err
+	}
+
+	return pool, nil
+}
